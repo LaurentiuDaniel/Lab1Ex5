a4d62c725d802ae770673bcd348f38669f5ccf50
Dani <amaximoaiei.l@yahoo.com>
2023-06-23 23:17:52 +0300

    Add project files.

---
+++ Lab1Ex5/Ex5Advanced.cs
@@ -0,0 +1,31 @@
+﻿/*Ex 5
+Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+• Exemplu: citim ,9,0 Afisam : 0 9 */
+
+internal class Ex5Advanced
+{
+
+    private static void Main()
+    {
+        double primulNumar;
+        double alDoileaNumar;
+
+
+        Console.WriteLine("Introdu primul numar:");
+        primulNumar = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introdu al doilea numar:");
+        alDoileaNumar = double.Parse(Console.ReadLine());
+
+        double[] doubleArray = new double[] { primulNumar, alDoileaNumar };
+        Array.Sort(doubleArray);
+        Console.WriteLine("Aceasta este ordinea sirului tau:");
+
+        foreach (double i in doubleArray)
+        {
+            Console.WriteLine(i + " ");
+        }
+
+    }
+
+}
---
+++ Lab1Ex5/Lab1Ex5.cs
@@ -0,0 +1,30 @@
+﻿
+/*Ex 5
+Se citesc doua numere de la tastatura, x, y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+• Exemplu: citim ,9,0 Afisam : 0 9*/
+
+class Lab1Ex5 {
+    static void Main()
+    {
+        int primulNumar;
+        int alDoileaNumar;
+
+
+        Console.WriteLine("Introdu primul numar:");
+        primulNumar = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introdu al doilea numar:");
+        alDoileaNumar = int.Parse(Console.ReadLine());
+
+        if (primulNumar > alDoileaNumar) {
+            Console.WriteLine("Ordinea ta este:");
+            Console.WriteLine(alDoileaNumar);
+            Console.WriteLine(primulNumar);
+        }
+        else {
+            Console.WriteLine("Ordinea ta este:");
+            Console.WriteLine(primulNumar);
+            Console.WriteLine(alDoileaNumar);
+        }
+    }
+}
